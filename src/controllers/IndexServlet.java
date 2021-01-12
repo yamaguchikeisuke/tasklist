5967a108bbb43d4303b544662005c9e8e6af1106
aniki <anikibifefefe@gmail.com>
2021-01-12 23:58:24 +0900

    Add getAllMessages

---
+++ src/controllers/IndexServlet.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Tasklist;
+import utils.DBUtil;
+
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public IndexServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Tasklist> tasks = em.createNamedQuery("getAlltasks", Tasklist.class).getResultList();
+        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+
+        em.close();
+    }
+
+}
