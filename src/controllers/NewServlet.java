6443b1102ba258a29aebcdcb1dd69b7737bda0c9
aniki <anikibifefefe@gmail.com>
2021-01-13 21:27:33 +0900

    Add NewServlet.java

---
+++ src/controllers/NewServlet.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Tasklist;
+import utils.DBUtil;
+
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public NewServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
+
+        Tasklist m = new Tasklist();
+
+        String content = "hello";
+        m.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        m.setCreated_at(currentTime);
+        m.setUpdated_at(currentTime);
+
+        em.persist(m);
+        em.getTransaction().commit();
+
+        response.getWriter().append(Integer.valueOf(m.getId()).toString());
+
+        em.close();
+    }
+
+}
